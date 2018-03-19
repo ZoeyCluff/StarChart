6a82d36fcdcb06f7d2316e3708582e213bc5f95a
Zoey Cluff <matada89@gmail.com>
2018-03-19 04:06:16 -0500

    got getTasks working

--- fetch.js
+++ fetch.js
@@ -1,23 +1,17 @@
 const knex = require('knex')(require('./knexfile'))
-var dataArr = [];
+
 
 module.exports = {
 
   GetTasks ({ username, userid }) {
-    var result = [];
+
     console.log(`Retrieving tasks for ${username} with user ID ${userid}`)
 
     return knex('tasks').select('task', 'due').where('userid', `${userid}`).then(function(result) {
-      result.forEach(function(value) {
-        dataArr.push(value)
-        console.log(JSON.stringify(result))
-      })
-      console.log("outside" + " " + JSON.stringify(result))
-        return dataArr
-
+      return result
     })
 
-    return dataArr
+
   }
 
 }
